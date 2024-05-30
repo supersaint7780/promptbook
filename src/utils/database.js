7b9008095b7c77af779289f3a6bfaa15125e36c8
Ayush Maurya <amaurya5556@gmail.com>
2024-05-30 18:08:31 +0530

    Add google login

---
+++ src/utils/database.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+let isConnected = false; // track the connection status
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("MongoDB is already connected");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "promptbook",
+    });
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log("Connection Error:", error);
+  }
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      boxShadow: {
+        "custom-inset": "inset 10px -50px 94px 0 rgba(199, 199, 199, 0.2)",
+      },
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "primary-orange": "#FF5722",
       },
     },
   },
